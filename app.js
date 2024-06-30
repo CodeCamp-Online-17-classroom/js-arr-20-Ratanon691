679b461cbb48434ab4b7bea0bcfdb85fecb6e192
Non T <117605943+Ratanon691@users.noreply.github.com>
2024-06-30 11:00:41 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
-// write code here
+function ask(question, yes, no) {
+  if (confirm(question)) yes();
+  else no();
+}
+ask(`Do you Agree?`, function(){ alert(`Yes, I agree`)}, function(){ alert(`Whyyyyy?`)})
